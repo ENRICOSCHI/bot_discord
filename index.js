5706c458ebca6a9233934dcdf408658998cffe49
ENRICOSCHI <fenrico472@gmail.com>
2022-09-26 18:57:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,12 +30,6 @@ client.on("messageCreate", (message) => {
    }
    /*if(message.content == "cazzo" || message.content=="Cazzo"|| message.content=="CAZZO"){
     message.channel.send("ATTENZIONE NON SI DICONO PAROLACCE!!")*/
-
-})
-
-
-//moderazione
-client.on("messageCreate", (message) =>{
     var parolacce=["cazzo", "merda","stronzo","fanculo","coglione","bimbominchia","porco", "dio", "madonna","negro"]
     var trovata= false;
 
@@ -53,6 +47,27 @@ client.on("messageCreate", (message) =>{
         message.channel.send({embeds: [embed]})
     }
 })
+
+
+//moderazione
+/*client.on("messageCreate", (message) =>{
+    var parolacce=["cazzo", "merda","stronzo","fanculo","coglione","bimbominchia","porco", "dio", "madonna","negro"]
+    var trovata= false;
+
+    parolacce.forEach(parola =>{
+        if(message.content.includes(parola)){
+            trovata= true;
+            testo=testo.replace(eval(`/${parola}/g`),"###");
+        }
+    })
+    if(trovata){
+        message.delete();
+        var embed= new Discord.MessageEmbed()
+            .setTitle("Hai detto una parola vietata nel server")
+            .setDescription("Hai scritto una parola vietata nella frase: " + testo)
+        message.channel.send({embeds: [embed]})
+    }
+})*/
 
 
 
