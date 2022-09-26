6c60678efb7affa38ab6f615c41019c43dfb74a1
ENRICOSCHI <fenrico472@gmail.com>
2022-09-26 20:53:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,36 +32,3 @@ client.on("messageCreate", (message) => {
    /*if(message.content == "cazzo" || message.content=="Cazzo"|| message.content=="CAZZO"){
     message.channel.send("ATTENZIONE NON SI DICONO PAROLACCE!!")*/
 })
-
-
-
-
-
-
-
-
-/*module.exports = {
-    name:'warn',
-    description: 'warn a user for some reason.',
-    run:async(client,message,args) =>{
-        if(!message.member.permission.has("MENAGE_MEBERS")) return message.reply({content: "**YOU DON'T HAVE PERMISSION TO USE THIS COMMAND**"})
-        
-        let user= message.mentions.members.first() || client.users.cache.get(args[0]);//mention a member to warn or warn the member by their user ID
-        let reason = args.slice(1).join(' ')  
-
-        if (!user) return message.reply({content: "Please specify a valid user to warn! "});
-        
-        let ember = new MessageEmbed()
-        .setAuthor({ name: message.author.tag, iconURL: user.displayAvatarURL({ dynamic: true })})
-        .setDescription(`{message.author} warned ${user} \n\n Reason: ${reason? reason: 'no reason provided'}`)
-        .setTimestamp()
-        .setFooter({text:`${user.tag || user.user.tag}`,iconURL: user.displayAvatarURL({dynamic: true}) || user.user.displayAvatarURL({dynamic:true})})
-        message.reply({embeds: [embed]}).then(()=>{
-            db.add(`warns_${user.id}`,1)//add one warning to database
-            let dm = new MessageEmbed()
-            .setDescription(`you were warned by ${message.author}\n\n Reason: ${reason ? reason : 'No reason provided'}`)
-            .setTimestamp()
-            user.send({embed: [embed]})//send a dm to the user that get warned
-        });
-    }
-};*/
