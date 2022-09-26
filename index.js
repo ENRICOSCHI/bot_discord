2e97ffe623d7c08c08840cc3d8126f39b269f992
Dominik K <dominik@koch-bautechnik.de>
2022-09-26 17:53:04 +0200

    Small Improvements!

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-const Discord = require("discord.js")
-const client = new Discord.Client(
+const { Client } = require("discord.js")
+const { ChalkAdvanced } = require('chalk-advanced');
+require("dotenv").config()
+const client = new Client(
     {intents:["GUILDS","GUILD_MEMBERS","GUILD_MESSAGES"]}
 )
 
 client.login(process.env.token)
 
 client.on("ready",() =>{
-    console.log("Bot Online")
+    console.log(`${ChalkAdvanced.green('Bot Online')}`);
 })
+
+const prefix = process.env.PREFIX || "!"
 
 
 client.on("messageCreate", (message) => {
-   if(message.content == "!youtube"){
-    message.channel.send("Il mio canale youtube: https://www.youtube.com/channel/UC-DzoW_FlAfbGyJgBK6PCHw")
+   if(message.content == prefix + "youtube"){
+    message.reply("Il mio canale youtube: https://www.youtube.com/channel/UC-DzoW_FlAfbGyJgBK6PCHw")
    }
-   if(message.content == "!twitch"){
-    message.channel.send("Il mio canale twitch: https://www.twitch.tv/enrycon")
+   if(message.content == prefix + "twitch"){
+    message.reply("Il mio canale twitch: https://www.twitch.tv/enrycon")
    }
-   if(message.content == "!twitter"){
-    message.channel.send("Il mio account twitter: https://twitter.com/ENRICOSCHI")
+   if(message.content == prefix + "twitter"){
+    message.reply("Il mio account twitter: https://twitter.com/ENRICOSCHI")
    }
-   if(message.content == "!scratch"){
-    message.channel.send("EHI! lo sapevi che ho creato dei giochi su scratch quando andavo alle medie? beh ora si; clicca qui per vedere le mie creazioni: https://scratch.mit.edu/users/enry234m/")
+   if(message.content == prefix + "scratch"){
+    message.reply("EHI! lo sapevi che ho creato dei giochi su scratch quando andavo alle medie? beh ora si; clicca qui per vedere le mie creazioni: https://scratch.mit.edu/users/enry234m/")
    }
    if(message.content == "ping!"){
-    message.channel.send("pong!")
+    message.reply("pong!")
    }
    if(message.content == "cazzo" || message.content=="Cazzo"|| message.content=="CAZZO"){
-    message.channel.send("ATTENZIONE NON SI DICONO PAROLACCE!!")
+    message.reply("ATTENZIONE NON SI DICONO PAROLACCE!!")
 }
 })
