1067a7e7b3429de4cc05e59dd1d8968aee14941a
ENRICOSCHI <fenrico472@gmail.com>
2022-09-26 18:46:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,14 +40,15 @@ client.on("messageCreate", (message) =>{
     parolacce.forEach(parola =>{
         if(message.content.includes(parola)){
             trovata= true;
-            testo=testo.replace(eval(`/${parola}/g`),"###");
+            //testo=testo.replace(eval(`/${parola}/g`),"###");
+            testo="ciao";
         }
     })
     if(trovata){
         message.delete();
         var embed= new Discord.MessageEmbed()
             .setTitle("Hai detto una parola vietata nel server")
-            .setDescription("Hai scritto una parola vietata"+testo)
+            .setDescription("Hai scritto una parola vietata nella frase: "+testo)
         message.channel.send({embeds: [embed]})
     }
 })
